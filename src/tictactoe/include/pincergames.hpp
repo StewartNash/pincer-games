1e19dea208cc7439831a871ab561b9e70ebdded0
pervigilent <pervigilent@gmail.com>
2025-02-04 11:12:48 -0700

    Added gitignore

--- src/tictactoe/include/pincergames.hpp
+++ src/tictactoe/include/pincergames.hpp
@@ -7,15 +7,43 @@
 
 namespace pincergames {
 
-const std::string ROBOT_WINS = "";
+const std::string ROBOT_WINS = "\
+  ____       *           *              _           \n\
+ |  * \\ *** | |**   ___ | |_  __      *(*)_ **  **_ \n\
+ | |_) / * \\| '* \\ / * \\| *_| \\ \\ /\\ / / | '_ \\/ __|\n\
+ |  * < (*) | |_) | (_) | |_   \\ V  V /| | | | \\__ \\\n\
+ |_| \\_\\___/|_.__/ \\___/ \\__|   \\_/\\_/ |_|_| |_|___/\
+";
 
-const std::string HUMAN_WINS = "";
+const std::string HUMAN_WINS = "\
+  *   *                                        _           \n\
+ | | | |_   * ***_**   ** ***** ***   __      *(*)_ **  **_ \n\
+ | |_| | | | | '_ `  \\ / ` | '_ \\  \\ \\ /\\ / / | '_ \\/ __|\n\
+ |  *  | |*| | | | | | | (_| | | | |  \\ V  V /| | | | \\__ \\\n\
+ |_| |_|\\__,_|_| |_| |_|\\__,_|_| |_|   \\_/\\_/ |_|_| |_|___/\
+";
 
-const std::string TIE_GAME = "";
+const std::string TIE_GAME = "\
+  ___ *   *               *   *      \n\
+ |_ *| |*( )___    __ *  | |*(_) ___ \n\
+  | || **|// **|  / *` | | *_| |/ _ \\\n\
+  | || |_  \\__ \\ | (_| | | |_| |  __/\n\
+ |___|\\__| |___/  \\__,_|  \\__|_|\\___|\
+";
 
 class TicTacToe : public rclcpp::Node {
 	public:
 		TicTacToe();
+		
+		char boardState[9];
+		char committedMoves[9];
+		char currentTurn;
+		short currentFigure;
+		char lastDetectedBoard[9];
+		bool waitingForHuman;
+		bool gameActive;
+		
+		
 }
 
 }
