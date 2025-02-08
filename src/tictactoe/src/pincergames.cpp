4f78c5bdfb23e91e8f93326b4263fee0f630050b
pervigilent <pervigilent@gmail.com>
2025-02-08 14:33:58 -0700

    Added positions.

--- src/tictactoe/src/pincergames.cpp
+++ src/tictactoe/src/pincergames.cpp
@@ -4,9 +4,67 @@ using namespace pincergames;
 //using namespace std::chrono_literals
 
 TicTacToe::TicTacToe() : rclcpp::Node("tic_tac_toe_robot"), count_(0) {
+	std::fill(std::begin(boardState), std::end(boardState), '\0');
+	std::fill(std::begin(committedMoves), std::end(committedMoves), '\0');
+	std::fill(std::begin(lastDetectedBoard), std::end(lastDetectedBoard), '\0');
+
+	currentTurn = 'X';
+	currentFigure = 1;
+	waitingForHuman = false;
+	gameActive = true;
+
+	inputThread = std::thread(&TicTacToe::handleKeyboardInput, this);
+
+	positions["1AP"] = {{{{-1.300, 1.049, -0.818}}, {{0.000, 0.110, -0.267}}}};
+	positions["1"] = {{{{-1.310, 1.197, -0.764}}, {{0.000, 0.312, -0.267}}}};
+	positions["2AP"] = {{{{-1.101, 1.049, -0.766}}, {{0.000, 0.075, -0.616}}}};
+	positions["2"] = {{{{-1.096, 1.183, -0.716}}, {{0.000, 0.232, -0.511}}}};
+	positions["3AP"] = {{{{-0.906, 1.049, -0.668}}, {{0.000, 0.075, -0.826}}}};
+	positions["3"] = {{{{-0.916, 1.171, -0.607}}, {{0.000, 0.075, -0.721}}}};
+	positions["4AP"] = {{{{-0.766, 1.049, -0.529}}, {{0.000, -0.099, -1.035}}}};
+	positions["4"] = {{{{-0.766, 1.173, -0.431}}, {{0.000, -0.099, -0.930}}}};
+	positions["5AP"] = {{{{-0.661, 1.066, -0.396}}, {{0.000, -0.099, -1.209}}}};
+	positions["5"] = {{{{-0.668, 1.199, -0.274}}, {{0.000, -0.274, -1.105}}}};
+	positions["T1AP"] = {{{{-0.888, 1.129, -0.103}}, {{0.000, -0.518, -0.930}}}};
+	positions["T1"] = {{{{-0.888, 1.247, -0.016}}, {{0.000, -0.518, -0.756}}}};
+	positions["T2AP"] = {{{{-1.004, 1.246, 0.265}}, {{0.000, -0.780, -0.756}}}};
+	positions["T2"] = {{{{-1.004, 1.373, 0.363}}, {{0.000, -0.780, -0.651}}}};
+	positions["T3AP"] = {{{{-1.080, 1.456, 0.754}}, {{0.000, -1.042, -0.581}}}};
+	positions["T3"] = {{{{-1.080, 1.595, 0.925}}, {{0.000, -1.077, -0.581}}}};
+	positions["T4AP"] = {{{{-1.087, 1.045, -0.335}}, {{0.000, -0.274, -0.581}}}};
+	positions["T4"] = {{{{-1.087, 1.193, -0.234}}, {{0.000, -0.379, -0.581}}}};
+	positions["T5AP"] = {{{{-1.164, 1.167, 0.066}}, {{0.000, -0.641, -0.581}}}};
+	positions["T5"] = {{{{-1.164, 1.289, 0.169}}, {{0.000, -0.641, -0.477}}}};
+	positions["T6AP"] = {{{{-1.230, 1.359, 0.534}}, {{0.000, -0.955, -0.477}}}};
+	positions["T6"] = {{{{-1.221, 1.471, 0.639}}, {{0.000, -0.955, -0.372}}}};
+	positions["T7AP"] = {{{{-1.314, 1.045, -0.431}}, {{0.000, -0.274, -0.232}}}};
+	positions["T7"] = {{{{-1.314, 1.176, -0.339}}, {{0.000, -0.274, -0.232}}}};
+	positions["T8AP"] = {{{{-1.370, 1.145, -0.042}}, {{0.000, -0.606, -0.302}}}};
+	positions["T8"] = {{{{-1.370, 1.258, 0.045}}, {{0.000, -0.606, -0.197}}}};
+	positions["T9AP"] = {{{{-1.387, 1.284, 0.412}}, {{0.000, -0.955, -0.302}}}};
+	positions["T9"] = {{{{-1.387, 1.429, 0.520}}, {{0.000, -0.955, -0.197}}}};
+	positions["TOP_CENTER"] = {{{{-0.136, 1.018, 1.639}}, {{0.000, -0.640, -0.581}}}};
+
+	resetGame();
+
+	clearTerminal();
+	displayBoard();
+	std::cout << "\nWaiting for moves... (X: Robot, O: Human)" << std::endl;
+	std::cout << "\nPress 'r' and Enter to restart the game at any time" << std::endl;
+
 	publisher_ = this->create_publisher<std_msgs::msg::String>("ui_command", 10);
 	//timer_ = this->create_wall_timer(500ms, std::bind(&TicTacToe::timer_callback, this));
 	timer_ = this->create_wall_timer(std::literals::chrono_literals::operator""ms(500), std::bind(&TicTacToe::timer_callback, this));
+}
+
+TicTacToe::~TicTacToe() {
+	stopThread.store(true);
+	if (inputThread.joinable()) {
+		inputThread.join();
+	}
+
+	publisher_.reset();
+	timer_.reset();
 }
 
 void TicTacToe::timer_callback() {
@@ -14,4 +72,41 @@ void TicTacToe::timer_callback() {
 	message.data = "TicTacToe " + std::to_string(count_++);
 	RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
 	publisher_->publish(message);
+}
+
+void TicTacToe::handleKeyboardInput() {
+	char userInput;
+	while (!stopThread.load()) {
+		std::cin >> userInput;
+		if (userInput == 'r') {
+			resetGame();
+			//break;
+		}
+	}
+}
+
+void TicTacToe::resetGame() {
+	std::fill(std::begin(boardState), std::end(boardState), '\0');
+	std::fill(std::begin(committedMoves), std::end(committedMoves), '\0');
+	std::fill(std::begin(lastDetectedBoard), std::end(lastDetectedBoard), '\0');
+
+	currentFigure = 1;
+	waitingForHuman = true;
+	gameActive = true;
+	
+	clearTerminal();
+	displayBoard();
+	std::cout << "\nGame Reset! Waiting for human move..." << std::endl;
+
+	clearTerminal();
+	displayBoard();
+	std::cout << "\nWaiting for moves... (X: Robot, O: Human)" << std::endl;
+}
+
+void TicTacToe::clearTerminal() {
+	std::cout << "\033[2J\033[1;1H";
+}
+
+void TicTacToe::displayBoard() {
+
 }
--- src/tictactoe/src/pincergames.hpp
+++ src/tictactoe/src/pincergames.hpp
@@ -4,6 +4,12 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <thread>
+#include <iostream>
+#include <algorithm>
+#include <atomic>
+#include <map>
+#include <array>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
@@ -36,20 +42,34 @@ const std::string TIE_GAME = "\
 
 class TicTacToe : public rclcpp::Node {
 	public:
-		TicTacToe();
+		static constexpr int NUMBER_OF_POSITIONS = 19;
 		
-		char boardState[9];
-		char committedMoves[9];
-		char currentTurn;
-		short currentFigure;
-		char lastDetectedBoard[9];
-		bool waitingForHuman;
+		TicTacToe();
+		~TicTacToe();
+
+		char boardState[9]; // Current visual state
+		char committedMoves[9]; // Memory of committed moves
+		char lastDetectedBoard[9]; // Store last detected board state
+		
+		char currentTurn; // X is robot and O is human
+		short currentFigure; // Track which figure to use next (1 - 5)		
 		bool gameActive;
+		bool waitingForHuman;
+		
+		std::map<std::string, std::array<std::array<double, 3>, NUMBER_OF_POSITIONS>> positions;
 	private:
 		void timer_callback();
 		rclcpp::TimerBase::SharedPtr timer_;
 		rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
 		size_t count_;
+		
+		std::thread inputThread; // Create keyboard input thread for reset
+		std::atomic<bool> stopThread{false};
+		
+		void handleKeyboardInput();
+		void resetGame();
+		void clearTerminal();
+		void displayBoard();
 };
 
 }
